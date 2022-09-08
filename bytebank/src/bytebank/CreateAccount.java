a8e1068c80e4f59948532b1c7c8193551ab8a849
Junior Sott <sottjr@gmail.com>
2022-09-08 22:50:18 +0200

    chore: add secondAccount

--- bytebank/src/bytebank/CreateAccount.java
+++ bytebank/src/bytebank/CreateAccount.java
@@ -5,5 +5,16 @@ public class CreateAccount {
 		Account firstAccount = new Account();
 		firstAccount.balance = 200;
 		System.out.println(firstAccount.balance);
+
+		firstAccount.balance += 100;
+		System.out.println(firstAccount.balance);
+
+		Account secondAccount = new Account();
+		secondAccount.balance = 50;
+		System.out.println(secondAccount.balance);
+
+		System.out.println("The first account have a balance of U$ " + firstAccount.balance);
+		System.out.println("The first account have a balance of U$ " + secondAccount.balance);
 	}
+
 }
