b7290664ed0c987589f7a15465fb44df7987d148
Junior Sott <sottjr@gmail.com>
2022-09-09 21:40:34 +0200

    chore: add second account

---
+++ bytebank/src/bytebank/TestReferences.java
@@ -0,0 +1,14 @@
+package bytebank;
+
+public class TestReferences {
+	public static void main(String[] args) {
+		Account firstAccount = new Account();
+		firstAccount.balance = 300;
+		
+		System.out.println("Balance of first Account: U$ " + firstAccount.balance);
+	
+		Account secondAccount = firstAccount;
+		
+		System.out.println("Balance of second Account: U$ " + secondAccount.balance);
+	}
+}
